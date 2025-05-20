1d3a16b59e017bf7140239ae42732d3ca96f42fe
Swarnanjali <chinthalaswarnanjali@gmail.com>
2025-05-20 16:17:51 +0530

    Add checkbox for password visibility and show error on invalid login

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -1,15 +1,29 @@
 import { useEffect, useState } from "react";
 import { FormContainer } from "./index.styles";
+import Cookies from "js-cookie";
+import { Navigate } from "react-router";
+
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState(""); 
+  const [error_msg, setError_msg] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const usernameinput = (event) => {
     setUsername(event.target.value);
   };
-  
+  const checkboxval = (event) => {
+    setShowPassword(event.target.checked);
+  };
   const passwordinput = (event) => {
     setPassword(event.target.value);
   };
+  const onSubmitSuccess = (jwt_token) => {
+    Cookies.set("jwt_token", jwt_token, { expires: 1 });
+   
+  }
+  const onSubmitFailure = (error_msg) => {
+    setError_msg(error_msg);
+  } 
 const submitForm = async (e) => {
   e.preventDefault();
 
@@ -22,8 +36,16 @@ const submitForm = async (e) => {
     const response = await fetch(url, options);
     const data = await response.json();
     console.log(data);
+    if(response.ok){
+      onSubmitSuccess(data.jwt_token)
+    }else{
+      onSubmitFailure(data.error_msg)
+    }
 };
-
+const jwtToken=Cookies.get("jwt_token")
+if(jwtToken !== undefined){
+  return <Navigate to="/" />
+}
   return (
     
     <FormContainer>
@@ -31,9 +53,14 @@ const submitForm = async (e) => {
       <form onSubmit={submitForm}>
         <input type="text" placeholder="Username" value={username} onChange={usernameinput} />
         <br/>
-        <input type="password" placeholder="Password" value={password} onChange={passwordinput} />
+        <input type={showPassword?"text":"password"} placeholder="Password" value={password} onChange={passwordinput} />
+        
+        <br/> 
+        <input type="checkbox" checked={showPassword} onChange={checkboxval} /> 
+        <label>Show Password</label>
         <br/>
         <button type="submit">Login</button>
+        <p>{error_msg}</p>
       </form>
     </FormContainer>
   );
